a1b11e261d2a03c85927a32cdd26ee1772c60b8c
Deni Gunawan <artdengun@gmail.com>
2022-08-01 13:44:16 +0700

    update

---
+++ src/main/java/com/deni/gunawan/backendspringreact/BackendSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.deni.gunawan.backendspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deni/gunawan/backendspringreact/BackendSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.gunawan.backendspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
